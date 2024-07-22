6307f4b9813ac163b6fb00fc7ff582121a3a21a3
Teijeiro Yugueros <laura.teijeiro-yugueros@capgemini.com>
2024-07-22 12:41:09 +0200

    nuevo

--- SQL/practica5.sql
+++ SQL/practica5.sql
@@ -3,7 +3,7 @@ SELECT CONCAT (LAST_NAME, ' earns ', SALARY, ' monthly but wants ', SALARY *3) A
 FROM EMPLOYEES;
 /
 --5.2
-SELECT LAST_NAME, HIRE_DATE, TO_CHAR(HIRE_DATE, 'DAY, "the" DD "of" MONTH, YYYY')
+SELECT LAST_NAME, HIRE_DATE, TO_CHAR(NEXT_DAY(ADD_MONTHS(HIRE_DATE,6),'MONDAY'),'fmDay, "the" DdSPTH "of" Month, YYYY')
 FROM EMPLOYEES;
 /
 --5.3
@@ -16,13 +16,13 @@ FROM EMPLOYEES;
 /
 --5.4
 SELECT JOB_ID,
-(CASE
-    WHEN JOB_ID = 'AD_PRES' THEN 'A'
-    WHEN JOB_ID = 'ST_MAN' THEN 'B'
-    WHEN JOB_ID = 'IT_PROG' THEN 'C'
-    WHEN JOB_ID = 'SA_REP' THEN 'D'
-    WHEN JOB_ID = 'ST_CLERK' THEN 'E'
-ELSE 'None of the above' END )
+CASE JOB_ID
+    WHEN  'AD_PRES' THEN 'A'
+    WHEN 'ST_MAN' THEN 'B'
+    WHEN 'IT_PROG' THEN 'C'
+    WHEN 'SA_REP' THEN 'D'
+    WHEN 'ST_CLERK' THEN 'E'
+ELSE 'None of the above' END 
 FROM EMPLOYEES;
 /
 --5.5
@@ -43,5 +43,6 @@ SELECT JOB_ID,
                     'ST_MAN', 'B',
                     'IT_PROG', 'C',
                     'SA_REP', 'D',
-                    'ST_CLERK', 'E')
+                    'ST_CLERK', 'E',
+                    'None of the above')
 FROM EMPLOYEES;
